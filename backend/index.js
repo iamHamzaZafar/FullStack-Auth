74cebfc44e4ae2ca573e5f71d0480ab8be5b7ffd
Hamza Zafar <link2hamzazafar@gmail.com>
2024-06-19 15:34:57 +0500

    connect to DB

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
-
+dotenv.config();
 const port = process.env.PORT || 5000;
 
+
+// Mongo db Connection
+try {
+  const connection = await mongoose
+    .connect(`${process.env.MONGODB_URI}/FullstackAuth`)
+    .then(() => {
+      console.log("Mongo Db Connection established!!!!!!");
+    });
+} catch (error) {
+  console.log("Connection error: " + error);
+  process.exit(1);
+}
+
+
+// server running
 app.listen(port, () => {
-  console.log(`Listening on ${port}`);
+  console.log(`Server Listening on PORT: ${port}`);
 });
